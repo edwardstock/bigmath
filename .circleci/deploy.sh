0f614f6ab26511af912ac2a66212ca6b632c68b4
Eduard Maximovich <edward.vstock@gmail.com>
2020-09-18 19:03:43 +0300

    temporary removed caching, added shared build, removed packaging for github

--- .circleci/deploy.sh
+++ .circleci/deploy.sh
@@ -14,11 +14,15 @@ fi
 ## Deploy in latest channel
 if [ "${sysname}" == "Linux" ]; then
   CONAN_LOCAL=1 conan create . edwardstock/latest -s compiler.libcxx=${stdlibname}11 -s build_type=Debug --build=missing
+  CONAN_LOCAL=1 conan create . edwardstock/latest -s compiler.libcxx=${stdlibname}11 -s build_type=Debug -o shared=True --build=missing
   CONAN_LOCAL=1 conan create . edwardstock/latest -s compiler.libcxx=${stdlibname}11 -s build_type=Release --build=missing
+  CONAN_LOCAL=1 conan create . edwardstock/latest -s compiler.libcxx=${stdlibname}11 -s build_type=Release -o shared=True --build=missing
 fi
 
 CONAN_LOCAL=1 conan create . edwardstock/latest -s compiler.libcxx=${stdlibname} -s build_type=Debug --build=missing
+CONAN_LOCAL=1 conan create . edwardstock/latest -s compiler.libcxx=${stdlibname} -s build_type=Debug -o shared=True --build=missing
 CONAN_LOCAL=1 conan create . edwardstock/latest -s compiler.libcxx=${stdlibname} -s build_type=Release --build=missing
+CONAN_LOCAL=1 conan create . edwardstock/latest -s compiler.libcxx=${stdlibname} -s build_type=Release -o shared=True --build=missing
 
 if [ "${NOUPLOAD}" != "1" ]; then
   CONAN_LOCAL=1 conan upload bigmath/${VERS}@edwardstock/latest --all -r=edwardstock
