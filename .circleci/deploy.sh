0737ee534e0222f58bec2c0ae36fbe6899364f84
Eduard Maximovich <edward.vstock@gmail.com>
2020-09-18 18:49:19 +0300

    removed typo

--- .circleci/deploy.sh
+++ .circleci/deploy.sh
@@ -16,7 +16,7 @@ stdlibname="libstdc++"
 if [ "${sysname}" == "Darwin" ]; then
   stdlibname="libc++"
 fi
-sud
+
 ## Deploy in latest channel
 if [ "${sysname}" == "Linux" ]; then
   CONAN_LOCAL=1 conan create . edwardstock/latest -s compiler.libcxx=${stdlibname}11 -s build_type=Debug --build=missing
