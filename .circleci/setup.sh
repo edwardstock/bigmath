78ac2563b0ae3bfba71692d2620be1bb1d8064b9
Eduard Maximovich <edward.vstock@gmail.com>
2021-01-14 13:51:31 +0300

    find cmake by which cmd too

--- .circleci/setup.sh
+++ .circleci/setup.sh
@@ -10,6 +10,8 @@ if [ ! -f "/tmp/pkgs/cmake.sh" ]; then
   wget -O /tmp/pkgs/cmake.sh https://github.com/Kitware/CMake/releases/download/v3.14.5/cmake-3.14.5-Linux-x86_64.sh
 fi
 
-if [ ! -f "/usr/bin/cmake" ]; then
+cmake_bin=$(which cmake)
+
+if  [ "${cmake_bin}" == "" ] && [ ! -f "/usr/bin/cmake" ]; then
   sh /tmp/pkgs/cmake.sh --skip-license --prefix=/usr
 fi
